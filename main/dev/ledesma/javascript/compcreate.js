adb6c6c63560a4b40e76125a6345d6d931190a11
adriansonaplane <109830657+adriansonaplane@users.noreply.github.com>
2022-08-21 15:21:54 -0700

    Finished Create Complaint Route

---
+++ main/dev/ledesma/javascript/compcreate.js
@@ -0,0 +1,30 @@
+const inputCategory = document.getElementById("category");
+const inputDesc = document.getElementById("description");
+
+
+document.addEventListener("submit", async event =>{
+
+    event.preventDefault();
+
+    const category = inputCategory.value;
+    const description = inputDesc.value;
+
+    const complaint = {id:0, category, description};
+    console.log(complaint);
+    const response = await fetch("http://localhost:8080/complaint",{
+        method:"POST",
+        body: JSON.stringify(complaint),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    if(response.status === 201){
+
+        alert("Successful");
+        inputCategory.value = "";
+        inputDesc.value = "";
+
+    }else{alert("Failed");}
+
+});
